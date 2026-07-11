166da7b612c66e0e178335ccf788f1844a629794
VeronicahWanjuu <v.wanjuu@alustudent.com>
2026-07-11 22:26:30 +0200

    Add about and contact routes

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
   res.send('Welcome to my Express server!');
 });
 
+app.get('/about', (req, res) => {
+  res.send('This is the about page for my Express server.');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('Contact us at: hello@example.com');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
